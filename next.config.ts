ec3b553fc608ba174cf2c1fc4aae42a9c1239e74
Mahim Tiwari <mahimt.official@gmail.com>
2025-06-16 17:52:46 +0530

    starting to make the user page and some backend for it

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+    images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
 };
 
 export default nextConfig;
